bb444c7aa77d4ab8a368d8b34b1681c6d3dbec40
sunxiaobin <xiaobinsun6@gmail.com>
2022-10-20 11:32:40 +0900

    commit02

--- Springboot_all/src/main/java/cn/springboot/SpringbootAllApplication.java
+++ Springboot_all/src/main/java/cn/springboot/SpringbootAllApplication.java
@@ -7,7 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootAllApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootAllApplication.class, args);
+		/*
+		 * `第一节课 springApplication 自定义
+		 * 把SpringApplication进行实例化 SpringApplication app = new SpringApplication(SpringbootAllApplication.class)
+		 * 可以调用SpringApplication中的好多方法，比如监听器等等
+		 * 
+		 */
+		SpringApplication app = new SpringApplication(SpringbootAllApplication.class);
+		app.run(args);
+		//SpringApplication.run(SpringbootAllApplication.class, args);
 	}
 
 }
